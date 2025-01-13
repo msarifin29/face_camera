d648d509ea06e5c3ef0222257243394c841e3d7c
samsul <ms.arifin29@gmail.com>
2025-01-13 11:55:25 +0800

    fix (package) : fixing version package

--- lib/src/paints/face_painter.dart
+++ lib/src/paints/face_painter.dart
@@ -22,14 +22,11 @@ class FacePainter extends CustomPainter {
 
     scaleX = size.width / imageSize.width;
     scaleY = size.height / imageSize.height;
-    canvas.drawRRect(
-        _scaleRect(
-          rect: face!.boundingBox,
-          widgetSize: size,
-          scaleX: scaleX,
-          scaleY: scaleY,
-        ),
-        paint);
+
+    canvas.drawPath(
+      _defaultPath(rect: face!.boundingBox, widgetSize: size, scaleX: scaleX, scaleY: scaleY),
+      paint,
+    );
   }
 
   @override
@@ -38,11 +35,24 @@ class FacePainter extends CustomPainter {
   }
 }
 
-RRect _scaleRect({required Rect rect, required Size widgetSize, double? scaleX, double? scaleY}) {
-  return RRect.fromLTRBR(
-      (widgetSize.width - rect.left.toDouble() * scaleX!),
-      rect.top.toDouble() * scaleY!,
-      widgetSize.width - rect.right.toDouble() * scaleX,
-      rect.bottom.toDouble() * scaleY,
-      const Radius.circular(10));
+Path _defaultPath({required Rect rect, required Size widgetSize, double? scaleX, double? scaleY}) {
+  double cornerExtension = 30.0;
+
+  double left = widgetSize.width - rect.left.toDouble() * scaleX!;
+  double right = widgetSize.width - rect.right.toDouble() * scaleX;
+  double top = rect.top.toDouble() * scaleY!;
+  double bottom = rect.bottom.toDouble() * scaleY;
+  return Path()
+    ..moveTo(left - cornerExtension, top)
+    ..lineTo(left, top)
+    ..lineTo(left, top + cornerExtension)
+    ..moveTo(right + cornerExtension, top)
+    ..lineTo(right, top)
+    ..lineTo(right, top + cornerExtension)
+    ..moveTo(left - cornerExtension, bottom)
+    ..lineTo(left, bottom)
+    ..lineTo(left, bottom - cornerExtension)
+    ..moveTo(right + cornerExtension, bottom)
+    ..lineTo(right, bottom)
+    ..lineTo(right, bottom - cornerExtension);
 }
